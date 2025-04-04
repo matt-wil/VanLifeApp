5d2859e45b870606f9317c38bf434650c77a502d
Matt Williams <matthew.steven.williams@gmail.com>
2025-04-04 07:23:39 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Vans from './pages/Vans'
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom"
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <header className='flex bg-amber-100 p-10 text-gray-700 br-10'>
+            <NavLink className="text-4xl" to="/">#VanLife</NavLink>
+          <nav className='flex gap-5 m-4 '>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/vans">Vans</NavLink>
+          </nav>
+        </header>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/about' element={<About />}/>
+          <Route path='/vans' element={<Vans />}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ViteLogo.jsx
@@ -0,0 +1,12 @@
+import viteLogo from '/vite.svg'
+
+
+const ViteLogo = () => {
+  return (
+            <a href="https://vite.dev" target="_blank">
+                <img src={viteLogo} className="logo" alt="Vite logo" />
+            </a>
+  )
+}
+
+export default ViteLogo
