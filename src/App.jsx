3f9d78283c1a3d05d45edec3881102b15f791a1c
Matt Williams <matthew.steven.williams@gmail.com>
2025-04-07 15:15:13 +0200

    import

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import About from "./pages/About"
 import Vans from "./pages/Vans"
 import VanDetail from "./pages/VanDetail"
 import CampgroundSearch from './pages/Campsites'
+import Layout from "./components/Layout"
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom"
 import './App.css'
 
@@ -11,13 +12,13 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-        <Route element={<Layout />}>
-          <Route path='/' element={<Home />}/>
-          <Route path='/about' element={<About />}/>
-          <Route path='/vans' element={<Vans />}/>
-          <Route path="/vans/:id" element={<VanDetail />} />
-          <Route path='/campsites' element={<CampgroundSearch />}/>
-        </Route>
+          <Route element={<Layout />}>
+            <Route path='/' element={<Home />}/>
+            <Route path='/about' element={<About />}/>
+            <Route path='/vans' element={<Vans />}/>
+            <Route path="/vans/:id" element={<VanDetail />} />
+            <Route path='/campsites' element={<CampgroundSearch />}/>
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
