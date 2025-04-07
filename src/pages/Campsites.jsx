16a13ea186facc399072ec85ac7806106754844c
Matt Williams <matthew.steven.williams@gmail.com>
2025-04-07 15:19:20 +0200

    add searchquery

--- src/pages/Campsites.jsx
+++ src/pages/Campsites.jsx
@@ -1,7 +1,7 @@
 // API call
 import React, { useEffect, useState } from 'react';
 
-function useCampgroundData(searchQuery) {
+function useCampgroundData(searchQuery="moutain") {
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
